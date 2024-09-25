94a8e86604b67d68570ee941d4a1517c5b41cb55
kanuni <mister.nicto006@gmail.com>
2024-09-25 10:54:32 +0300

    добавил кнопку для закрытия модального окна

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,18 +10,17 @@ export const Modal = () => {
 
   if (!isOpen) return null; // Если модальное окно не должно быть открыто, ничего не рендерим
 
-  // const handleClose = () => {
-  //   dispatch(closeModal()); // Закрываем модальное окно
+  const handleClose = () => {
+    dispatch(closeModal()); // Закрываем модальное окно
 
-  //   console.log(1233);
+    console.log(1233);
     
-  // };
+  };
   return (
     <div className="modal">
       <div className="modal-content">
-        {/* <button className="close-btn" onClick={handleClose}>
-          Close
-        </button> */}
+       {handleClose}
+       
         <AddPost />
       </div>
     </div>
