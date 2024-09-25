b15dfc213d8a1423cea55c6afd52f945fae43983
kanuni <mister.nicto006@gmail.com>
2024-09-25 10:54:32 +0300

    добавил кнопку для закрытия модального окна

--- src/components/AddPost/AddPost.jsx
+++ src/components/AddPost/AddPost.jsx
@@ -53,6 +53,11 @@ export const AddPost = () => {
     console.log(123);
   };
 
+  const handleClickClose = ()=>{
+
+    dispatch(closeModal())
+  }
+
   const handleChangeFile = (e) => {
     const target = e.target;
     const selectedFile = target.files[0];
@@ -95,9 +100,12 @@ export const AddPost = () => {
           </div>
         </div>
 
-        <form className="addPost-btn">
+        <form className="addPost-form">
           <Button variant="addPost-btn" fullWidth onClick={handleClick}>
             Add
+          </Button>
+          <Button variant="closePost-btn" fullWidth onClick={handleClickClose}>
+            Cloze
           </Button>
         </form>
       </div>
